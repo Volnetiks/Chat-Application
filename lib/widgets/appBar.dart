eb8fc5e5863f07f86ba40d60824550ac3e3e6fa6
Volnetiks - Clémentine <volnetiks@gmail.com>
2020-07-19 18:02:40 +0200

    Implemented appBar

--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -4,21 +4,39 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget title;
   final List<Widget> actions;
   final Widget leading;
-  final bool centerTile;
+  final bool centerTitle;
 
   CustomAppBar({
     Key key,
     @required this.title,
     @required this.actions,
     @required this.leading,
-    @required this.centerTile,
+    @required this.centerTitle,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.pink,
+            width: 1.4,
+            style: BorderStyle.solid
+          )
+        )
+      ),
+      child: AppBar(
+        elevation: 0,
+        leading: leading,
+        actions: actions,
+        centerTitle: centerTitle,
+        title: title,
+      ),
+    );
   }
 
   @override
-  Size get preferredSize => throw UnimplementedError();
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 10);
 }
