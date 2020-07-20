7fb0dff93fbeb15f36458bba9caa4013be0e188d
Volnetiks - Clémentine <volnetiks@gmail.com>
2020-07-20 09:50:20 +0200

    Added gradient_app_bar package and GradientAppBar in search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,9 @@
 import 'package:chat_application_backend/models/user.dart';
 import 'package:chat_application_backend/resources/firebase_repository.dart';
+import 'package:chat_application_backend/widgets/appBar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -12,7 +14,7 @@ class _SearchScreenState extends State<SearchScreen> {
 
   FirebaseRepository _repository = FirebaseRepository();
 
-  List<User> userList;
+  List<User> users;
   String query = "";
   TextEditingController searchController = TextEditingController();
 
@@ -21,12 +23,63 @@ class _SearchScreenState extends State<SearchScreen> {
     super.initState();
 
     _repository.getCurrentUser().then((FirebaseUser user) {
-
+      _repository.fetchAllUsers(user).then((List<User> userList) {
+        setState(() {
+          users = userList;
+        });
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: GradientAppBar(
+        gradient: LinearGradient(colors: [Colors.red, Colors.purple]),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight + 20),
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: searchController,
+              onChanged: (val) {
+                setState(() {
+                  query = val;
+                });
+              },
+              cursorColor: Colors.grey,
+              autofocus: true,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 35
+              ),
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.close, color: Colors.white),
+                  onPressed: () {
+                    searchController.clear();
+                  },
+                ),
+                border: InputBorder.none,
+                hintText: "Search...",
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                  color: Colors.grey,
+                )
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
